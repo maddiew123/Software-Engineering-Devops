bac4282ffaf588e772aeb1f41f12da6105990b48
maddie w <madeleine.w_2023_lon@outlook.com>
2026-03-16 17:54:25 +0000

    security

--- backend/main.py
+++ backend/main.py
@@ -27,4 +27,4 @@ if __name__ == "__main__":
     import uvicorn
     import os
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)  # nosec B104
